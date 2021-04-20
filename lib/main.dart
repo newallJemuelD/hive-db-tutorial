76e4bcf5f8f31f6539f0f8818935b030f0328521
Shalini <newalljemuel.d@gmail.com>
2021-04-20 18:04:01 +0530

    initiation and closing of the local storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 import 'contact_page.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  final appDocumentDirectory =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Hive Tutorial',
-      home: ContactPage(),
+      home: FutureBuilder(
+        future: Hive.openBox('contacts'),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError)
+              return Text(snapshot.error.toString());
+            else
+              return ContactPage();
+          } else
+            return Scaffold();
+        },
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    Hive.close();
+    super.dispose();
   }
 }
